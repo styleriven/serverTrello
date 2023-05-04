66e16d8a07530cafb163a9c30d2480a0b885fcce
Duc Hoang <duchoang206h@gmail.com>
2023-05-04 22:54:54 +0700

    feat: enable cors

--- trello/settings.py
+++ trello/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
      'rest_framework_simplejwt.token_blacklist',
     "myapp",
     "rest_framework",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,10 +53,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "trello.urls"
-
+CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
